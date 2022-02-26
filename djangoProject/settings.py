95c1bcc4a4873d6b4f8581665cbe3751f8274cc2
StudentDuS <1005839481@qq.com>
2022-02-26 20:13:54 +0800

    v1.0

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodata',  # 数据库名称
+        'USER': 'root',        # 用户名
+        'PASSWORD': '123',     # 密码
+        'HOST': '127.0.0.1',   # 数据库地址
+        'PORT': 3306,          # 端口
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
